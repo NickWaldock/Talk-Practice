9f2a69845977e5757a5f62b5e16a724e3f399490
Nick Waldock <106591977+NickWaldock@users.noreply.github.com>
2023-03-08 16:26:28 +0000

    Create list post view to render a list of posts

--- talk_practice/blog/views.py
+++ talk_practice/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import BlogPost
+
 
 # Create your views here.
+def post_list(request):
+    list = BlogPost.objects.all()
+    context = {
+        "list": list
+    }
+    return render(request, "post-list.html", context)

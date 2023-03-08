228c68d39962abb62535d0598ac5c0b58c43386f
Nick Waldock <106591977+NickWaldock@users.noreply.github.com>
2023-03-08 14:17:14 +0000

    Set up django, create first BlogPost model

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Talk-Practice
  A forum site for musicians to discuss practice
+
+
+References
+Django crash course
+https://www.youtube.com/watch?v=Lx5-XtMGDsA
---
+++ talk_practice/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=150)
+    author = models.CharField(max_length=20)
+    date_updated = models.DateField()
+    blog_content = models.TextField()
+    
